ef6efed6554acf9577ec9e80e4160ab2509f7292
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-06-11 23:40:42 +0100

    Update WwiseSoundbankParser.cpp

--- WwiseSoundbankParser.cpp
+++ WwiseSoundbankParser.cpp
@@ -1,3 +1,5 @@
+// ©2023 JDSherbert
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -5,9 +7,11 @@
 
 using namespace rapidxml;
 
-void parseSoundbank(const std::string& xmlFilePath) {
+void parseSoundbank(const std::string& xmlFilePath) 
+{
     std::ifstream file(xmlFilePath);
-    if (!file.is_open()) {
+    if (!file.is_open()) 
+    {
         std::cout << "Failed to open XML file." << std::endl;
         return;
     }
@@ -15,22 +19,28 @@ void parseSoundbank(const std::string& xmlFilePath) {
     std::vector<char> buffer((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
     buffer.push_back('\0');
 
-    xml_document<> doc;
+    xml_document<> soundbankXml;
     doc.parse<0>(&buffer[0]);
 
-    xml_node<>* soundbankInfoNode = doc.first_node("SoundBank")->first_node("SoundBankInfo");
+    xml_node<>* soundbankInfoNode = soundbankXml.first_node("SoundBank")->first_node("SoundBankInfo");
     std::string soundbankName = soundbankInfoNode->first_node("ShortName")->value();
-    std::cout << "SoundBank Name: " << soundbankName << std::endl;
+    
+}
 
-    xml_node<>* eventNode = doc.first_node("SoundBank")->first_node("Events")->first_node("Event");
-    while (eventNode != nullptr) {
+std::map<std::string, std::string> GetEvents(xml_document<>& soundbankXml)
+{   
+    std::map<std::string, std::string> events;
+
+    xml_node<>* eventNode = soundbankXml.first_node("SoundBank")->first_node("Events")->first_node("Event");
+    while (eventNode != nullptr) 
+    {
         std::string eventName = eventNode->first_node("Name")->value();
         std::string eventID = eventNode->first_node("ID")->value();
-
-        std::cout << "Event Name: " << eventName << std::endl;
-        std::cout << "Event ID: " << eventID << std::endl;
-        std::cout << std::endl;
-
+        
+        events[eventID] = eventName;
+       
         eventNode = eventNode->next_sibling("Event");
     }
+    
+    return events;
 }
