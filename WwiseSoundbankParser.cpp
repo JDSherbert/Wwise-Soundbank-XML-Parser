3efadddbeaaf2972e02df213f905b1dd2f7cf04a
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-06-12 00:46:56 +0100

    Update WwiseSoundbankParser.cpp

--- WwiseSoundbankParser.cpp
+++ WwiseSoundbankParser.cpp
@@ -1,46 +1,126 @@
 // ©2023 JDSherbert
 
 #include <iostream>
-#include <fstream>
+#include <map>
 #include <string>
-#include "rapidxml.hpp"
+#include <tinyxml2.h>
 
-using namespace rapidxml;
-
-void parseSoundbank(const std::string& xmlFilePath) 
+std::map<std::string, std::string> GetEventIDAndName(const std::string& soundbankXmlPath)
 {
-    std::ifstream file(xmlFilePath);
-    if (!file.is_open()) 
+    std::map<std::string, std::string> eventMap;
+
+    tinyxml2::XMLDocument doc;
+    doc.LoadFile(soundbankXmlPath.c_str());
+
+    tinyxml2::XMLElement* eventElement = doc.FirstChildElement("SoundBank")->FirstChildElement("Events")->FirstChildElement("Event");
+    while (eventElement)
     {
-        std::cout << "Failed to open XML file." << std::endl;
-        return;
+        const char* eventID = eventElement->FirstChildElement("ID")->GetText();
+        const char* eventName = eventElement->FirstChildElement("Name")->GetText();
+
+        if (eventID && eventName)
+        {
+            eventMap[eventID] = eventName;
+        }
+
+        eventElement = eventElement->NextSiblingElement("Event");
     }
 
-    std::vector<char> buffer((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-    buffer.push_back('\0');
-
-    xml_document<> soundbankXml;
-    doc.parse<0>(&buffer[0]);
-
-    xml_node<>* soundbankInfoNode = soundbankXml.first_node("SoundBank")->first_node("SoundBankInfo");
-    std::string soundbankName = soundbankInfoNode->first_node("ShortName")->value();
-    
+    return eventMap;
 }
 
-std::map<std::string, std::string> GetEvents(xml_document<>& soundbankXml)
-{   
-    std::map<std::string, std::string> events;
+std::map<std::string, std::string> GetRTPCIDAndName(const std::string& soundbankXmlPath)
+{
+    std::map<std::string, std::string> rtpcMap;
 
-    xml_node<>* eventNode = soundbankXml.first_node("SoundBank")->first_node("Events")->first_node("Event");
-    while (eventNode != nullptr) 
+    tinyxml2::XMLDocument doc;
+    doc.LoadFile(soundbankXmlPath.c_str());
+
+    tinyxml2::XMLElement* rtpcElement = doc.FirstChildElement("SoundBank")->FirstChildElement("RealTimeParameters")->FirstChildElement("RealTimeParameter");
+    while (rtpcElement)
     {
-        std::string eventName = eventNode->first_node("Name")->value();
-        std::string eventID = eventNode->first_node("ID")->value();
-        
-        events[eventID] = eventName;
-       
-        eventNode = eventNode->next_sibling("Event");
+        const char* rtpcID = rtpcElement->FirstChildElement("ID")->GetText();
+        const char* rtpcName = rtpcElement->FirstChildElement("Name")->GetText();
+
+        if (rtpcID && rtpcName)
+        {
+            rtpcMap[rtpcID] = rtpcName;
+        }
+
+        rtpcElement = rtpcElement->NextSiblingElement("RealTimeParameter");
     }
-    
-    return events;
+
+    return rtpcMap;
+}
+
+std::map<std::string, std::string> GetSwitchIDAndName(const std::string& soundbankXmlPath)
+{
+    std::map<std::string, std::string> switchMap;
+
+    tinyxml2::XMLDocument doc;
+    doc.LoadFile(soundbankXmlPath.c_str());
+
+    tinyxml2::XMLElement* switchElement = doc.FirstChildElement("SoundBank")->FirstChildElement("Switches")->FirstChildElement("Switch");
+    while (switchElement)
+    {
+        const char* switchID = switchElement->FirstChildElement("ID")->GetText();
+        const char* switchName = switchElement->FirstChildElement("Name")->GetText();
+
+        if (switchID && switchName)
+        {
+            switchMap[switchID] = switchName;
+        }
+
+        switchElement = switchElement->NextSiblingElement("Switch");
+    }
+
+    return switchMap;
+}
+
+std::map<std::string, std::string> GetStateIDAndName(const std::string& soundbankXmlPath)
+{
+    std::map<std::string, std::string> stateMap;
+
+    tinyxml2::XMLDocument doc;
+    doc.LoadFile(soundbankXmlPath.c_str());
+
+    tinyxml2::XMLElement* stateElement = doc.FirstChildElement("SoundBank")->FirstChildElement("States")->FirstChildElement("State");
+    while (stateElement)
+    {
+        const char* stateID = stateElement->FirstChildElement("ID")->GetText();
+        const char* stateName = stateElement->FirstChildElement("Name")->GetText();
+
+        if (stateID && stateName)
+        {
+            stateMap[stateID] = stateName;
+        }
+
+        stateElement = stateElement->NextSiblingElement("State");
+    }
+
+    return stateMap;
+}
+
+std::map<std::string, std::string> GetAuxBusIDAndName(const std::string& soundbankXmlPath)
+{
+    std::map<std::string, std::string> auxBusMap;
+
+    tinyxml2::XMLDocument doc;
+    doc.LoadFile(soundbankXmlPath.c_str());
+
+    tinyxml2::XMLElement* auxBusElement = doc.FirstChildElement("SoundBank")->FirstChildElement("AuxBus")->FirstChildElement("AuxBus");
+    while (auxBusElement)
+    {
+        const char* auxBusID = auxBusElement->FirstChildElement("ID")->GetText();
+        const char* auxBusName = auxBusElement->FirstChildElement("Name")->GetText();
+
+        if (auxBusID && auxBusName)
+        {
+            auxBusMap[auxBusID] = auxBusName;
+        }
+
+        auxBusElement = auxBusElement->NextSiblingElement("AuxBus");
+    }
+
+    return auxBusMap;
 }
