774c8b4e6f0bc4f467f4d74b35e023878b74052f
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-06-12 01:55:11 +0100

    Update WwiseSoundbankXMLParser.java

--- Java/WwiseSoundbankXMLParser.java
+++ Java/WwiseSoundbankXMLParser.java
@@ -1,3 +1,4 @@
+// ©2023 JDSherbert
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -72,4 +73,97 @@ public class WwiseSoundbankParser {
         }
 
         return rtpcMap;
+    }
+
+    public static Map<String, String> getSwitchIDAndName(String soundbankXmlPath) {
+        Map<String, String> switchMap = new HashMap<>();
+
+        try {
+            File soundbankXmlFile = new File(soundbankXmlPath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(soundbankXmlFile);
+
+            doc.getDocumentElement().normalize();
+            NodeList switchList = doc.getElementsByTagName("Switch");
+
+            for (int i = 0; i < switchList.getLength(); i++) {
+                Node switchNode = switchList.item(i);
+                if (switchNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element switchElement = (Element) switchNode;
+                    String switchID = switchElement.getElementsByTagName("ID").item(0).getTextContent();
+                    String switchName = switchElement.getElementsByTagName("Name").item(0).getTextContent();
+
+                    if (switchID != null && switchName != null) {
+                        switchMap.put(switchID, switchName);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return switchMap;
+    }
+
+    public static Map<String, String> getStateIDAndName(String soundbankXmlPath) {
+        Map<String, String> stateMap = new HashMap<>();
+
+        try {
+            File soundbankXmlFile = new File(soundbankXmlPath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(soundbankXmlFile);
+
+            doc.getDocumentElement().normalize();
+            NodeList stateList = doc.getElementsByTagName("State");
+
+            for (int i = 0; i < stateList.getLength(); i++) {
+                Node stateNode = stateList.item(i);
+                if (stateNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element stateElement = (Element) stateNode;
+                    String stateID = stateElement.getElementsByTagName("ID").item(0).getTextContent();
+                    String stateName = stateElement.getElementsByTagName("Name").item(0).getTextContent();
+
+                    if (stateID != null && stateName != null) {
+                        stateMap.put(stateID, stateName);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return stateMap;
+    }
+
+    public static Map<String, String> getAuxBusIDAndName(String soundbankXmlPath) {
+        Map<String, String> auxBusMap = new HashMap<>();
+
+        try {
+            File soundbankXmlFile = new File(soundbankXmlPath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(soundbankXmlFile);
+
+            doc.getDocumentElement().normalize();
+            NodeList auxBusList = doc.getElementsByTagName("AuxBus");
+
+            for (int i = 0; i < auxBusList.getLength(); i++) {
+                Node auxBusNode = auxBusList.item(i);
+                if (auxBusNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element auxBusElement = (Element) auxBusNode;
+                    String auxBusID = auxBusElement.getElementsByTagName("ID").item(0).getTextContent();
+                    String auxBusName = auxBusElement.getElementsByTagName("Name").item(0).getTextContent();
+
+                    if (auxBusID != null && auxBusName != null) {
+                        auxBusMap.put(auxBusID, auxBusName);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return auxBusMap;
     }
